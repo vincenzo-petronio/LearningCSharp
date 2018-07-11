5a163af5b499f28fff1b615560c61cfc0740e497
vincenzo-petronio <vincenzo.petronio@gmail.com>
2018-07-11 09:48:57 +0200

    ADD task, async, await, yield, lock

---
+++ 003_ConsoleReadQuotes/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace _003_ConsoleReadQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // nel Main non potendo usare il modificatore async come firma del metodo 
+            // devo utilizzare Wait(). Da C# 7.1 è possibile invece utilizzare async/await.
+            RunPrompt().Wait();
+        }
+
+        private static async Task RunPrompt()
+        {
+            var config = new PromptConfig();
+
+            var showTask = ShowPrompt(config);
+            var quotesTask = GetSpeed(config);
+
+            await Task.WhenAll(showTask, quotesTask);
+        }
+
+        private static async Task ShowPrompt(PromptConfig config)
+        {
+            var words = ReadFrom("quotes.txt");
+            foreach (string w in words)
+            {
+                Console.Write(w);
+                if (!String.IsNullOrWhiteSpace(w))
+                {
+                    await Task.Delay(config.DelayInMs);
+                }
+            }
+        }
+
+        private static async Task GetSpeed(PromptConfig config)
+        {
+            Action work = () =>
+            {
+                do
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == '+')
+                        config.UpdateDelay(-50);
+                    else if (key.KeyChar == '-')
+                        config.UpdateDelay(50);
+                } while (!config.GetDone());
+            };
+            await Task.Run(work);
+        }
+
+        private static IEnumerable<string> ReadFrom(string fileName)
+        {
+            string line;
+            using (var reader = File.OpenText(fileName))
+            {
+                while ((line = reader.ReadLine()) != null)
+                {
+                    //yield return line;
+                    var words = line.Split(' ');
+                    foreach (string word in words)
+                    {
+                        yield return word + " ";
+                    }
+
+                    // Usare questo invece di \n !!!
+                    yield return Environment.NewLine;
+                }
+            }
+        }
+    }
+}
---
+++ 003_ConsoleReadQuotes/PromptConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _003_ConsoleReadQuotes
+{
+    internal class PromptConfig
+    {
+        private Object objLockHandle = new Object();
+        public int DelayInMs { get; private set; } = 200;
+
+        private bool done;
+
+        public bool GetDone()
+        {
+            return done;
+        }
+
+        private void SetDone(bool value)
+        {
+            done = value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stepInMs">int less for fast</param>
+        public void UpdateDelay(int stepInMs)
+        {
+            var newDelay = Math.Min(DelayInMs + stepInMs, 1000);
+            newDelay = Math.Max(newDelay, 20);
+            lock (objLockHandle)
+            {
+                DelayInMs = newDelay;
+            }
+        }
+
+    }
+}
