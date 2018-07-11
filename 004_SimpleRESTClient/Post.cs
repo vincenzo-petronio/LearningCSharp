a66fac8c6415fb42cc73d1bfe53596602410dce2
vincenzo-petronio <vincenzo.petronio@gmail.com>
2018-07-11 10:47:33 +0200

    ADD example REST, API, serialization, json

---
+++ 004_SimpleRESTClient/Post.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace _004_SimpleRESTClient
+{
+    [DataContract]
+    class Post
+    {
+        [DataMember(Name = "userId")]
+        public string UserId { get; internal set; }
+
+        [DataMember(Name = "id")]
+        public int Id { get; internal set; }
+
+        [DataMember(Name = "title")]
+        public string Title { get; internal set; }
+
+        [DataMember(Name = "body")]
+        public string Body { get; internal set; }
+
+    }
+}
---
+++ 004_SimpleRESTClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+
+namespace _004_SimpleRESTClient
+{
+    /// <summary>
+    /// Chiamata ad API Rest con serializzazione.
+    /// </summary>
+    class Program
+    {
+        private static readonly HttpClient mHttpClient = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            GetApiData().Wait();
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+
+        private static async Task GetApiData()
+        {
+            var dataStringJson = await mHttpClient.GetStringAsync("http://jsonplaceholder.typicode.com/posts");
+            Console.WriteLine(dataStringJson);
+            Console.Write(Environment.NewLine);
+
+            var serializer = new DataContractJsonSerializer(typeof(List<Post>));
+            var dataStreamJson = await mHttpClient.GetStreamAsync("http://jsonplaceholder.typicode.com/posts");
+            List<Post> posts = serializer.ReadObject(dataStreamJson) as List<Post>;
+            foreach (Post p in posts)
+            {
+                Console.WriteLine(p.Id + " " + p.Title);
+            }
+        }
+    }
+}
