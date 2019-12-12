959bce0a656507dcd083d0cb35678704e335e402
Vincenzo Petronio <vincenzo.petronio@gmail.com>
2019-12-12 10:02:40 +0100

    ADD Local Functions example

---
+++ 010_LocalFunctions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _010_LocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Spammer spammer = new Spammer();
+            spammer.Start();
+        }
+    }
+}
---
+++ 010_LocalFunctions/Spammer.cs
@@ -0,0 +1,50 @@
+﻿using StandardSharedCode;
+using System;
+
+namespace _010_LocalFunctions
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Spammer
+    {
+        private string Message { get; set; }
+
+        public Spammer()
+        {
+
+        }
+
+        public void Start()
+        {
+            for (int i = 1; i < 10000; i++)
+            {
+                Message = $"SPAM_{i.ToString()}";
+                SendMessages();
+            }
+
+            Utils.BloccaConsole();
+        }
+
+        private void SendMessages()
+        {
+            if (IsValid(Message))
+            {
+                Print();
+            }
+            else
+            {
+                Console.WriteLine("TOO MUCH SPAM!" + " " + Message);
+            }
+
+            // LOCAL FUNCTION
+            void Print()
+            {
+                Console.WriteLine(Message);
+            }
+
+            // LOCAL FUNCTION + BODY EXPRESSION
+            bool IsValid(string msg) => !string.IsNullOrEmpty(msg) && msg.Length <= 8;
+        }
+    }
+}
